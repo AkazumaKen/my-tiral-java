60f2b27a7e67c09db332c7f74082244840a24b01
AkazumaKen <164737454+AkazumaKen@users.noreply.github.com>
2024-08-31 22:24:15 +0800

    Add files via upload

---
+++ math.java
@@ -0,0 +1,16 @@
+public class math {
+    
+
+
+    public static void main(String[] args){
+
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.abs(y);
+
+        System.out.println(z);
+
+    }
+
+}
